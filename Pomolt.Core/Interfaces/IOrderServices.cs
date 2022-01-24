e925bf6f0471e0dc43d43aae7e7be030df68fde1
Yakov Gurevich <umanrh@gmail.com>
2022-01-24 16:02:26 +0200

    OrdersServices resolved

---
+++ Pomolt.Core/Interfaces/IOrderServices.cs
@@ -0,0 +1,16 @@
+﻿using Promolt.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promolt.Core.Interfaces
+{
+    public interface IOrderServices
+    {
+        Task CreateOrder(OrderModel order);
+        Task<OrderModel> GetOrder(string id);
+        Task<List<OrderModel>> GetOrders();
+    }
+}
--- Pomolt.Core/Models/OrderModel.cs
+++ Pomolt.Core/Models/OrderModel.cs
@@ -8,15 +8,18 @@ using System.Threading.Tasks;
 
 namespace Promolt.Core.Models
 {
-    public class AccountModel
+    public class OrderModel
     {
 
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
-        public decimal Cash { get; set; }
+        public List<ItemModel> Cart { get; set; } = null!;
+        
+        public decimal? TotalPrice { get; set; }
+
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Owner { get; set; } = null!;
-        public string SocialUserName { get; set; } = null!;
+        public string CreatedBy { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
     }
 }
