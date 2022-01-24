820b7c8ec9664e763c8148efc3a209fe4b8e550a
Yakov Gurevich <umanrh@gmail.com>
2022-01-24 16:25:23 +0200

    Client_DonationServices added

---
+++ Client/Interfaces/IDonationServices.cs
@@ -0,0 +1,17 @@
+﻿using Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Interfaces
+{
+    public interface IDonationServices
+    {
+        Task CreateDonation(DonationModel Donation);
+        
+        Task<DonationModel> GetDonation(string id);
+        Task<List<DonationModel>> GetDonations();
+    }
+}
